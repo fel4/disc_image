6de66f91ae059e99d3dc0be35ae929a3909f2b0b
Cary Haynie <cary.haynie@gmail.com>
2017-04-22 10:30:14 -0400

    add iso number declarations.

--- src/numbers.rs
+++ src/numbers.rs
@@ -1,20 +1,4 @@
-#[allow(non_camel_case_types)]
-enum NumberFormat {
-    int8(u8),
-    sint8(i8),
-    int16_LSB(u16),
-    int16_MSB(u16),
-    int16_LSBMSB(u16, u16),
-    sint16_LSB(i16),
-    sint16_MSB(i16),
-    sint16_LSBMSB(i16,i16),
-    int32_LSB(u32),
-    int32_MSB(u32),
-    int32_LSBMSB(u32,u32),
-    sint32_LSB(i32),
-    sint32_MSB(i32),
-    sint32_LSBMSB(i32,i32)
-}
+#![allow(non_camel_case_types)]
 
 pub enum Endianness {
     Little,
@@ -22,6 +6,36 @@ pub enum Endianness {
     LittleBig
 }
 
-pub trait ISONumber {
+trait ISONumber {
+    fn endianness(&self) -> Endianness;
+}
 
+macro_rules! number {
+    ( $name:ident; $($sz:ty),+; $en:ident ) => (
+        pub struct $name($($sz,)+);
+
+        impl ISONumber for $name {
+            fn endianness(&self) -> Endianness { $crate::numbers::Endianness::$en }
+        } 
+    )
+}
+
+number!(sint8; i8; Little);
+number!(int8; u8; Little);
+number!(sint16_LSB; i16; Little);
+number!(sint16_MSB; i16; Big);
+number!(sint16_LSBMSB; i16,i16; LittleBig);
+number!(int16_LSB; u16; Little);
+number!(int16_MSB; u16; Big);
+number!(int16_LSBMSB; u16,u16; LittleBig);
+number!(sint32_LSB; i32; Little);
+number!(sint32_MSB; i32; Big);
+number!(sint32_LSBMSB; i32,i32; LittleBig);
+number!(int32_LSB; u32; Little);
+number!(int32_MSB; u32; Big);
+number!(int32_LSBMSB; u32,u32; LittleBig);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
