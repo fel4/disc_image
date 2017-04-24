a41005ad995b2068fe15c71ea5a9f00e9d993114
Cary Haynie <cary.haynie@gmail.com>
2017-04-24 15:11:36 -0400

    replace structs with nom macros in numbers module. Add (failing/empty) tests.

--- src/numbers.rs
+++ src/numbers.rs
@@ -1,41 +1,131 @@
-#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 
-pub enum Endianness {
-    Little,
-    Big,
-    LittleBig
-}
+use nom;
 
-trait ISONumber {
-    fn endianness(&self) -> Endianness;
-}
-
-macro_rules! number {
-    ( $name:ident; $($sz:ty),+; $en:ident ) => (
-        pub struct $name($($sz,)+);
-
-        impl ISONumber for $name {
-            fn endianness(&self) -> Endianness { $crate::numbers::Endianness::$en }
-        } 
+named!(pub sint8<i8>, call!(nom::le_i8));
+named!(pub int8<u8>, call!(nom::le_u8));
+named!(pub sint16_LSB<i16>, i16!(nom::Endianness::Little));
+named!(pub sint16_MSB<i16>, i16!(nom::Endianness::Big));
+named!(pub sint16_LSBMSB<(i16,i16)>,
+    do_parse!(
+        x: call!(sint16_LSB) >>
+        y: call!(sint16_MSB) >>
+        (x,y)
     )
-}
-
-number!(sint8; i8; Little);
-number!(int8; u8; Little);
-number!(sint16_LSB; i16; Little);
-number!(sint16_MSB; i16; Big);
-number!(sint16_LSBMSB; i16,i16; LittleBig);
-number!(int16_LSB; u16; Little);
-number!(int16_MSB; u16; Big);
-number!(int16_LSBMSB; u16,u16; LittleBig);
-number!(sint32_LSB; i32; Little);
-number!(sint32_MSB; i32; Big);
-number!(sint32_LSBMSB; i32,i32; LittleBig);
-number!(int32_LSB; u32; Little);
-number!(int32_MSB; u32; Big);
-number!(int32_LSBMSB; u32,u32; LittleBig);
+);
+named!(pub int16_LSB<u16>, u16!(nom::Endianness::Little));
+named!(pub int16_MSB<u16>, u16!(nom::Endianness::Big));
+named!(pub int16_LSBMSB<(u16, u16)>,
+    do_parse!(
+        x: call!(int16_LSB) >>
+        y: call!(int16_MSB) >>
+        (x,y)
+    )
+);
+named!(pub sint32_LSB<i32>, i32!(nom::Endianness::Little));
+named!(pub sint32_MSB<i32>, i32!(nom::Endianness::Big));
+named!(pub sint32_LSBMSB<(i32,i32)>,
+    do_parse!(
+        x: call!(sint32_LSB) >>
+        y: call!(sint32_MSB) >>
+        (x,y)
+    )
+);
+named!(pub int32_LSB<u32>, u32!(nom::Endianness::Little));
+named!(pub int32_MSB<u32>, u32!(nom::Endianness::Big));
+named!(pub int32_LSBMSB<(u32, u32)>,
+    do_parse!(
+        x: call!(int32_LSB) >>
+        y: call!(int32_MSB) >>
+        (x,y)
+    )
+);
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn sint8_valid() { unimplemented!(); }
+
+    #[test]
+    fn sint8_invalid() { unimplemented!(); }
+
+    #[test]
+    fn sint16LSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn sint16LSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn sint16MSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn sint16MSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn sint16LSBMSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn sint16LSBMSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn sint32LSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn sint32LSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn sint32MSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn sint32MSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn sint32LSBMSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn sint32LSBMSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn int8_valid() { unimplemented!(); }
+
+    #[test]
+    fn int8_invalid() { unimplemented!(); }
+
+    #[test]
+    fn int16LSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn int16LSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn int16MSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn int16MSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn int16LSBMSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn int16LSBMSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn int32LSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn int32LSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn int32MSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn int32MSB_invalid() { unimplemented!(); }
+
+    #[test]
+    fn int32LSBMSB_valid() { unimplemented!(); }
+
+    #[test]
+    fn int32LSBMSB_invalid() { unimplemented!(); }
 }
